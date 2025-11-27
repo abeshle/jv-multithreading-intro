3f0635c83ed1b7c1005808dc59aca6a5e8fff198
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2025-11-27 00:37:46 -0400

    Created threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread t1 = new MyThread();
+        t1.start();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread t2 = new Thread(myRunnable);
+        t2.setDaemon(true);
+        t2.start();
     }
 }
